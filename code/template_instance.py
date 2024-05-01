cb56a9c9269dc64caec7af63f1bbd1c78e3f30a9
Martin HA <martin.herrerias@gmail.com>
2024-05-01 16:27:46 +0100

    template_instance.py improvements
    NAME taken from template file, TARGET includes NAME.ext, jinja2 whitespace opts, extensive tests

--- code/template_instance.py
+++ code/template_instance.py
@@ -5,12 +5,11 @@ Command-line tool for creating instances of input/config files for BIDS
 workflows, based on a template.
 
 Renders a Jinja2 TEMPLATE file, replacing tokens {{sub}}, {{ses}},
-{{name}}, and {{basedir}} and writing the result to TARGET/NAME.EXT
+{{name}}, and {{basedir}} and writing the result to a TARGET file
 
-By default, the TARGET directory is 'derivatives/SUB/NAME/', and NAME is
-extracted from the template's `cwl:tool: path/to/NAME.cwl` basename. The
-TARGET directory will be created if it does not exist. The file extension
-EXT is preserved from the template.
+By default, the TARGET is 'derivatives/SUB/SES/NAME', where NAME is the
+TEMPLATE base-name (with .jinja* extension removed, if existing).
+The parent TARGET directory will be created if it does not exist.
 
 Paths for TEMPLATE and TARGET can be relative to BASEDIR. For consistency,
 the {{basedir}} token should be used in the TEMPLATE file for any relative
@@ -20,41 +19,37 @@ paths, as these may change depending on TARGET.
 import argparse
 import jinja2
 import os
-import re
 
 parser = argparse.ArgumentParser(description=__doc__,
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
 parser.add_argument("--sub", required=True, help="Subject label ('sub-' prefix will be added if missing)")
 parser.add_argument("--ses", required=True, help="Session label ('ses-' prefix will be added if missing)")
 parser.add_argument("--template", required=True, help="Input-object template")
-parser.add_argument("--target", required=False, default="derivatives/{{sub}}/{{name}}/",
-                    help="Target directory (Jinja2 template string), default: '%(default)s'")
+parser.add_argument("--target", required=False, default="derivatives/{{sub}}/{{ses}}/{{name}}",
+                    help="Target file path (Jinja2 template string), default: '%(default)s'")
 parser.add_argument("--name", required=False, default=None,
-                    help="Workflow name, extracted from the template's `cwl:tool` basename, if missing")
+                    help="Workflow name (default: base-name of TEMPLATE, without .jinja* extension)")
 parser.add_argument("--basedir", required=False, default=None, help="Base directory for relative paths")
+
+# remove whitespace from Jinja2 blocks
+JINJA_OPTS = {"trim_blocks": True, "lstrip_blocks": True}
 
 def create_instance(template, sub, ses, target = parser.get_default('target'), name = None, basedir = None):
 
-  if basedir:
-    os.chdir(basedir)
-  basedir = os.getcwd()
-
-  # Load the template file
-  with open(template, "r") as f:
-    template_str = f.read()
-
   # Extract the workflow name from the template, if missing
   if name is None:
-    match = re.search(r'cwl:tool:.*/(.*)\.cwl', template_str)
-    if match is None:
-      raise ValueError("Could not extract workflow name from template")
-    else:
-      name = match.group(1)
+    name = os.path.basename(template)
+    if "jinja" in os.path.splitext(name)[1].lower():
+      name = os.path.splitext(name)[0]
 
+  # Parse replacement tokens
   if not sub.startswith("sub-"):
     sub = "sub-" + sub
   if not ses.startswith("ses-"):
     ses = "ses-" + ses
+
+  if basedir is None:
+    basedir = "."
 
   tokens = {
     "sub": sub,
@@ -63,26 +58,37 @@ def create_instance(template, sub, ses, target = parser.get_default('target'), n
     "basedir": basedir
   }
 
-  # Create the target directory
-  target_dir = jinja2.Template(target).render(tokens)
-  os.makedirs(target_dir, exist_ok=True)
+  # Parse target, basedir, and template paths
+  target = jinja2.Template(target, **JINJA_OPTS).render(tokens)
 
-  target_dir = os.path.abspath(target_dir)
+  basedir = abs_path(basedir)
+  template = abs_path(template, basedir)
+  target = abs_path(target, basedir)
+
+  # Make sure the replacement `basedir` token is relative to target
+  target_dir = os.path.dirname(target)
   tokens["basedir"] = os.path.relpath(basedir, target_dir)
-  
-  # Render the template
-  instance = jinja2.Template(template_str).render(tokens)
 
-  # Write the instance to a file
-  ext = os.path.splitext(template)[1]
-  out_file = os.path.join(target_dir, name + ext)
-  with open(out_file, "w") as f:
+  # Render the template
+  with open(template, "r") as f:
+    template_str = f.read()
+
+  instance = jinja2.Template(template_str, **JINJA_OPTS).render(tokens)
+
+  os.makedirs(target_dir, exist_ok=True)
+  with open(target, "w") as f:
     f.write(instance)
 
-  print("Created instance at", out_file)
-  return out_file
+  print("Created instance at", target)
+  return target
+
+def abs_path(path, basedir = ""):
+  path = os.path.expanduser(os.path.expandvars(path))
+  if not os.path.isabs(path):
+      path = os.path.abspath(os.path.join(basedir, path))
+  return path
 
 if __name__ == "__main__":
   args = parser.parse_args()
-  create_instance(args.template, args.sub, args.ses, args.target, args.name, args.basedir)
+  create_instance(**args)
 
--- tests/test_template_instance.py
+++ tests/test_template_instance.py
@@ -2,45 +2,118 @@ import pytest
 import os
 import shutil
 import tempfile
+from dataclasses import dataclass, field
+import textwrap
 
 from ..code.template_instance import create_instance
 
-@pytest.fixture()
-def template():
+@dataclass
+class TemplateTest:
+    id: str # test id
+    contents: str = ""
+    parsed_contents: str = ""
+    args: dict = field(default_factory=dict)
+    sub: str = "A"
+    ses: str = "01"
+    name: str = "workflow.yml"
+    template: str = "code/workflow.yml.jinja"
+    parsed_target: str = "derivatives/sub-A/ses-01/workflow.yml"
 
-    template = """
-{# this is a comment #}
-#!/usr/bin/env cwl-runner
-cwl:tool: {{basedir}}/code/workflow.cwl
-foo: {class: File, path: {{basedir}}/rawdata/{{sub}}/{{ses}}/foo.txt}
-"""
-    sub = "A"
-    ses = "01"
-    name = "workflow"
+    def __post_init__(self):
+        args = {
+            "sub": self.sub,
+            "ses": self.ses,
+            "template": self.template
+        }
+        self.args.update(args)
 
-    instance = """
-#!/usr/bin/env cwl-runner
-cwl:tool: ../../../code/workflow.cwl
-foo: {class: File, path: ../../../rawdata/sub-A/ses-01/foo.txt}
-"""
-    template_file = os.path.join("code", name + ".yml")
+        self.contents = textwrap.dedent(self.contents)
+        self.parsed_contents = textwrap.dedent(self.parsed_contents)
 
-    temp_dir = tempfile.mkdtemp()
-    os.makedirs(os.path.join(temp_dir, os.path.dirname(template_file)), exist_ok=False)
+template_tests = [
+    TemplateTest("empty"),
+    TemplateTest("name", 
+                 args = {"name": "foo.bar",
+                         "target": "some/other/path/{{name}}"},
+                 parsed_target = "some/other/path/foo.bar"),
+    TemplateTest("root",
+        contents = "{{basedir}}/code/workflow.cwl",
+        parsed_contents = "./code/workflow.cwl",
+        args = {"target": "{{name}}"},
+        parsed_target = "workflow.yml"
+    ),
+    TemplateTest("yml",
+        contents = """
+        #!/usr/bin/env cwl-runner
+        cwl:tool: {{basedir}}/code/workflow.cwl
+        foo: {class: File, path: {{basedir}}/rawdata/{{sub}}/{{ses}}/foo.txt}
+        """,
+        parsed_contents = """
+        #!/usr/bin/env cwl-runner
+        cwl:tool: ../../../code/workflow.cwl
+        foo: {class: File, path: ../../../rawdata/sub-A/ses-01/foo.txt}
+        """
+    ),
+    TemplateTest("json",
+        contents = """
+        #!/usr/bin/env cwl-runner
+        {
+          "cwl:tool": "{{basedir}}/code/workflow.cwl",
+          "foo": {"class": "File", "path": "{{basedir}}/rawdata/{{sub}}/{{ses}}/foo.txt"}}
+        }
+        """,
+        parsed_contents = """
+        #!/usr/bin/env cwl-runner
+        {
+          "cwl:tool": "../../../code/workflow.cwl",
+          "foo": {"class": "File", "path": "../../../rawdata/sub-A/ses-01/foo.txt"}}
+        }
+        """,
+        template = "code/workflow.json.jinja",
+        parsed_target = "derivatives/sub-A/ses-01/workflow.json"
+    ),
+    TemplateTest("fancy",
+        contents = """
+        {% macro ses_data() -%}
+            {{basedir}}/rawdata/{{sub}}/{{ses}}
+        {%- endmacro %}
+        file_array:
+        {% for j in [1,2,3] %}
+          - class: File
+            path: {{ses_data()}}/file_{{j}}.txt
+        {% endfor %}
+        """,
+        parsed_contents = """
+        file_array:
+          - class: File
+            path: ../../../rawdata/sub-A/ses-01/file_1.txt
+          - class: File
+            path: ../../../rawdata/sub-A/ses-01/file_2.txt
+          - class: File
+            path: ../../../rawdata/sub-A/ses-01/file_3.txt
+        """
+    )
+]
 
-    with open(os.path.join(temp_dir, template_file), 'w') as f:
-        f.write(template)
-    
-    yield temp_dir, template_file, sub, ses, name, instance
+@pytest.fixture(params=template_tests, ids=[t.id for t in template_tests])
+def template(request):
+    T = request.param
+
+    temp_dir = tempfile.mkdtemp(prefix = T.id + '_')
+    os.makedirs(os.path.join(temp_dir, os.path.dirname(T.template)), exist_ok=True)
+    with open(os.path.join(temp_dir, T.template), 'w') as f:
+        f.write(T.contents)
+    yield temp_dir, T
     shutil.rmtree(temp_dir)
 
 def test_create_instance(template):
 
-    temp_dir, template_file, sub, ses, name, instance = template
+    temp_dir, T = template
 
     # Call the function to create an instance
-    out_file = create_instance(template_file, sub, ses, basedir = temp_dir)
-    assert out_file == os.path.join(temp_dir, "derivatives", "sub-" + sub, name, name + ".yml")
+    out_file = create_instance(**T.args, basedir=temp_dir)
+    
+    assert out_file == os.path.join(temp_dir, T.parsed_target)
     assert os.path.exists(out_file)
     with open(out_file, 'r') as f:
-        assert f.read().strip() == instance.strip()
+        assert f.read().strip() == T.parsed_contents.strip()
